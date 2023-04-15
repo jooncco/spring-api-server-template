d8fe724bc0168ba85856c19179760a538b800736
JunHa Daniel Jeong <jooncco.g@gmail.com>
2023-04-15 12:30:58 +0900

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-api-server-template
+🐬 Assets for spring api server that will ease your pain, aiming to deliver best practice in business product.
---
+++ src/test/java/com/template/springapiserver/SpringApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.springapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
